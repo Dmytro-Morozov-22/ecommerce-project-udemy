207c9466bb9cf2d07f1f637180f732335b6066b6
Dmytro-Morozov-22 <morozovdima6@gmail.com>
2024-02-28 23:21:14 +0200

    Installed Okta SDK dependencies, integrated Okta sign-in widget and created login component.

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oafccxvj8g5FIOn35d7',
+        issuer: 'https://dev-64134078.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
