ac3e409d577349527f6d2fdd24318099d0c23150
Dmytro-Morozov-22 <morozovdima6@gmail.com>
2024-03-07 00:06:43 +0200

    Developed login status component for login/logout buttons. Tried to update App Module configs to connect routes but faced with error - OktaAuthModule does not appear to be an NgModule class.

--- 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
-import { OktaSignin } from '@okta/okta-signin-widget'
+import { OktaSignIn } from '@okta/okta-signin-widget'
 import myAppConfig from 'src/app/config/my-app-config';
 
 @Component({
@@ -11,10 +11,10 @@ import myAppConfig from 'src/app/config/my-app-config';
 })
 export class LoginComponent {
 
-  oktaSignin: any;
+  oktaSignIn: any;
 
   constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth){
-    this.oktaSignin = new OktaSignin({
+    this.oktaSignIn = new OktaSignIn({
       logo: 'assets/images/logo.png',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
       clientId: myAppConfig.oidc.clientId,
@@ -28,8 +28,8 @@ export class LoginComponent {
   }
 
   ngOnInit(): void {
-    this.oktaSignin.remove();
-    this.oktaSignin.renderEl(
+    this.oktaSignIn.remove();
+    this.oktaSignIn.renderEl(
       {el: '#okta-signin-widget'}, // this name should be same as div tag id in login.component.html
       (response: any) => {
         if (response.status === 'SUCCESS') {
