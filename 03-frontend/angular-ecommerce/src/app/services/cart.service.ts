b39f6407328e12861119e9796bfac67b7baff930
Dmytro-Morozov-22 <morozovdima6@gmail.com>
2024-03-24 00:27:28 +0200

    Changed the length of the input search field from 935px to 600px because the member and logout buttons were not evenly positioned. Added new method findByEmail(...) to CustomerRepository. Updated CheckoutServiceImpl namely checked if the customer already exists if so then use the existing customer.

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -6,13 +6,26 @@ import { BehaviorSubject, Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class CartService {
-  
+
   cartItems: CartItem[] = [];
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
+  storage: Storage = localStorage; //sessionStorage
 
-  constructor() { }
+  constructor() {
+
+    //read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+
+    if (data != null) { 
+      this.cartItems = data;
+
+      // compute totals based on the data that is read from storage
+      this.computeCartTotals();
+    }
+
+  }
 
   addToCart(theCartItem: CartItem) {
 
@@ -24,7 +37,7 @@ export class CartService {
     if (this.cartItems.length > 0) {
 
       // find the item in the cart based on item id
-      existingCartItem = this.cartItems.find( tempCartItem => tempCartItem.id === theCartItem.id)!;
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id)!;
 
       // check if we found it
       alreadyExistsInCart = (existingCartItem != undefined);
@@ -41,9 +54,9 @@ export class CartService {
 
     //compute cart total price and total quantity
     this.computeCartTotals();
-      
+
   }
-  
+
   computeCartTotals() {
     let totalPriceValue: number = 0;
     let totalQuantityValue: number = 0;
@@ -60,17 +73,26 @@ export class CartService {
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
 
+    //persist cart data
+    this.persistCartItems();
   }
 
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+
+
+
+
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
-   console.log('Contents of the cart');
-   for (let tempCartItem of this.cartItems) {
-    const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
-    console.log(`name: ${tempCartItem.name}, quantity=${tempCartItem.quantity}, 
+    console.log('Contents of the cart');
+    for (let tempCartItem of this.cartItems) {
+      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
+      console.log(`name: ${tempCartItem.name}, quantity=${tempCartItem.quantity}, 
                  unitPrice=${tempCartItem.unitPrice}, subTotalPrice=${subTotalPrice}`);
-   }
-   console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantityValue: ${totalQuantityValue}`);
-   console.log('----');
+    }
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantityValue: ${totalQuantityValue}`);
+    console.log('----');
   }
 
   decrementQuantity(theCartItem: CartItem) {
@@ -86,7 +108,7 @@ export class CartService {
 
   remove(theCartItem: CartItem) {
     // get index of item in the array
-    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id);
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
 
     // if found, remove the item from the array at the given index
     if (itemIndex > -1) {
