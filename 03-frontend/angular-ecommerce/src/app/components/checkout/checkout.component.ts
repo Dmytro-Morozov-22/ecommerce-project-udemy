b598f7098afcbf106cb4a3d39db673db0ab112cd
Dmytro-Morozov-22 <morozovdima6@gmail.com>
2024-05-06 21:13:00 +0300

    The following changes were added to disable the purchase button to prevent multiple form submissions. Updated checkout.component.ts: 1. Added a Boolean field isDisabled, 2. Before REST API calls, set isDisabled to true, Once REST API calls complete, set isDisabled to false. Updated checkout.component.html: 1. Bind disabled attribute to isDisabled field.

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -42,6 +42,8 @@ export class CheckoutComponent implements OnInit {
   cardElement: any;
   displayError: any = "";
 
+  isDisabled: boolean = false; 
+
   constructor(private formBuilder: FormBuilder,
               private lun2ShopFormService: Luv2ShopFormService,
               private cartService: CartService,
@@ -282,6 +284,8 @@ export class CheckoutComponent implements OnInit {
 
   if (!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
 
+    this.isDisabled = true;
+
     this.checkoutService.createPaymentIntent(this.paymentInfo).subscribe(
       (paymentIntentResponse) => {
         this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
@@ -305,6 +309,7 @@ export class CheckoutComponent implements OnInit {
           if (result.error) {
             // inform the customer there was an error
             alert(`There was an error: ${result.error.message}`);
+            this.isDisabled = false;
           } else {
             // call REST API via the CheckoutService
             this.checkoutService.placeOrder(purchase).subscribe({
@@ -313,9 +318,11 @@ export class CheckoutComponent implements OnInit {
 
                 // reset cart
                 this.resetCart();
+                this.isDisabled = false;
               },
               error: (err: any) => {
                 alert(`There was an error: ${err.message}`);
+                this.isDisabled = false;
               }
             })
           }            
